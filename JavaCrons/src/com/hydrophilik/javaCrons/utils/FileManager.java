7e96a35db0ccf565c3b682165a031693d0d2fc6f
sbeslow <bezzy21@yahoo.com>
2014-09-18 10:31:56 -0500

    Method to write to text file

--- JavaCrons/src/com/hydrophilik/javaCrons/utils/FileManager.java
+++ JavaCrons/src/com/hydrophilik/javaCrons/utils/FileManager.java
@@ -1,14 +1,11 @@
 package com.hydrophilik.javaCrons.utils;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by scottbeslow on 9/5/14.
- */
 public class FileManager {
 
     public static List<String> readFileLines(String filePathAndName) {
@@ -37,5 +34,44 @@ public class FileManager {
             }
         }
         return retLines;
+    }
+
+    public static String createAndWriteToFile(String filePathAndName, List<String> fileLines) {
+
+        File file = new File(filePathAndName);
+
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+
+        // if file doesnt exists, then create it
+        if (file.exists()) {
+            if (!file.delete()) {
+                return "Unable to delete existing file: " + filePathAndName;
+            }
+        }
+
+        try {
+            if (!file.createNewFile())
+                return "Unable to create the file: " + filePathAndName;
+
+            fw = new FileWriter(file.getAbsoluteFile());
+            bw = new BufferedWriter(fw);
+
+            for (String line : fileLines) {
+                bw.write(line + "\n");
+            }
+        } catch (IOException e) {
+            return ExceptionUtils.getMessage(e);
+        }
+
+        finally {
+            try {
+                if (null != bw) bw.close();
+                if (null != fw) fw.close();
+            } catch(Exception e) {e.printStackTrace();}
+        }
+
+
+        return null;
     }
 }
